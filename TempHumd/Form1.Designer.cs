c011833eaac3ec05a0b5847f7af1a02580abc3c7
wisnukusuma <33643189+wisnukusuma@users.noreply.github.com>
2018-08-04 13:20:04 +0700

    Add files via upload

---
+++ TempHumd/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace TempHumd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.myport = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(766, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1016;
+            this.label1.Text = "COM-PORT";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(750, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 21);
+            this.comboBox1.TabIndex = 1014;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(750, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 38);
+            this.button3.TabIndex = 1013;
+            this.button3.Text = "STOP CONECTION";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(750, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 38);
+            this.button1.TabIndex = 1011;
+            this.button1.Text = "START CONECTION";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // myport
+            // 
+            this.myport.PortName = "COM4";
+            this.myport.ReceivedBytesThreshold = 14;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Suhu1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Suhu2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(704, 296);
+            this.chart1.TabIndex = 1034;
+            this.chart1.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(867, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(446, 150);
+            this.dataGridView1.TabIndex = 1035;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Node";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Date";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Temperature";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Humidity";
+            this.Column4.Name = "Column4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(749, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 38);
+            this.button2.TabIndex = 1036;
+            this.button2.Text = "GRAPH";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(0, 314);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Kelembaban1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Kelembaban2";
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(704, 296);
+            this.chart2.TabIndex = 1037;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1354, 614);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort myport;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        internal System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ TempHumd/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace TempHumd
+{
+    public partial class Form1 : Form
+    {
+        private string data;
+        private string data1, data3, data5;
+        private int data2, data4, data6;
+        //private string data3;
+        private int _baudRate = 9600;
+        private int _dataBits = 8;
+        private Handshake _handshake = Handshake.None;
+        private Parity _parity = Parity.None;
+        private StopBits _stopBits = StopBits.One;
+        int row = 0;
+        int[] suhu1, suhu2;
+        int[] kelembaban1, kelembaban2;
+        int i = 0;
+        int k = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //button2.Enabled = true;
+            button3.Enabled = false;
+            suhu1 = new Int32[50];
+            suhu2 = new Int32[50];
+            kelembaban1 = new Int32[50];
+            kelembaban2 = new Int32[50];
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            ldPort();
+            setSerial();
+        }
+
+        private void setSerial()
+        {
+            myport = new SerialPort();
+            myport.BaudRate = _baudRate;
+            myport.Parity = _parity;
+            myport.Handshake = _handshake;
+            myport.DataBits = _dataBits;
+            myport.StopBits = _stopBits;
+        }
+
+        private void ldPort()
+        {
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            if (ArrayComPortsNames.Length != 0)
+            {
+                do
+                {
+                    index += 1;
+                    comboBox1.Items.Add(ArrayComPortsNames[index]);
+                }
+                while (!((ArrayComPortsNames[index] == ComPortName) || (index == ArrayComPortsNames.GetUpperBound(0))));
+                Array.Sort(ArrayComPortsNames);
+                if (index == ArrayComPortsNames.GetUpperBound(0))
+                {
+                    ComPortName = ArrayComPortsNames[0];
+                }
+                comboBox1.Text = ArrayComPortsNames[0];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            for (int j = 1; j <= i; j++)
+            {
+                
+                chart1.Series["Suhu1"].Points.AddXY(j, suhu1[j]);
+                chart1.Series["Suhu2"].Points.AddXY(j, suhu2[j]);
+
+            }
+            for (int j = 1; j <= k; j++)
+            {
+
+                chart2.Series["Kelembaban1"].Points.AddXY(j, kelembaban1[j]);
+                chart2.Series["Kelembaban2"].Points.AddXY(j, kelembaban2[j]);
+
+            }
+
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            ldPort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (myport.IsOpen) myport.Close();
+                myport.PortName = comboBox1.Text;
+                myport.Open();
+                
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Port tidak dapat dibuka " + er.ToString(), "Buka Port Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (myport.IsOpen) { button1.Enabled = false; button3.Enabled = true; comboBox1.Enabled = false; myport.DataReceived += terima; }
+
+        }
+
+        void terima(object sender, SerialDataReceivedEventArgs e)
+        {
+            data = myport.ReadLine();
+            
+        
+            if (data.Substring(0, 1) == "1" && data.Substring (30,1)=="#")
+            {
+                i++;
+                data1 = data.Substring(2, 18);
+                //data2 = Convert.ToInt32(data1);
+                data3 = data.Substring(21, 4);
+                data4 = Convert.ToInt32(data3)/100;
+                data5 = data.Substring(26, 4);
+                data6 = Convert.ToInt32(data5)/100;
+                this.Invoke((MethodInvoker)delegate {
+                    dataGridView1.Rows.Add();
+                    row = dataGridView1.Rows.Count - 2;
+                    dataGridView1["Column1", row].Value = 1;
+                    dataGridView1["Column2", row].Value = data1;
+                    dataGridView1["Column3", row].Value = data4; suhu1[i] = data4;
+                    dataGridView1["Column4", row].Value = data6; kelembaban1[i] = data6;
+                });
+             }
+            else if (data.Substring(0, 1) == "2" && data.Substring(30, 1) == "#")
+            {
+                k++; 
+                data1 = data.Substring(2, 18);
+                //data2 = Convert.ToInt32(data1);
+                data3 = data.Substring(21, 4);
+                data4 = Convert.ToInt32(data3)/100;
+                data5 = data.Substring(26, 4);
+                data6 = Convert.ToInt32(data5)/100;
+                this.Invoke((MethodInvoker)delegate {
+                    dataGridView1.Rows.Add();
+                    row = dataGridView1.Rows.Count - 2;
+                    dataGridView1["Column1", row].Value = 2;
+                    dataGridView1["Column2", row].Value = data1;
+                    dataGridView1["Column3", row].Value = data4; suhu2[k] = data4;
+                    dataGridView1["Column4", row].Value = data6; kelembaban2[k] = data6;
+                });
+            }
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            //button2.Enabled = true;
+            button3.Enabled = false;
+
+            myport.Close();
+        }        
+    }
+}
